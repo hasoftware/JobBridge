ae38debbb89a0ff5edea9bffaa4d7e5a9badce81
tung1883 <nguyenductung1883@gmail.com>
2026-03-29 21:14:23 +0700

    fix random email test bị trùng

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,12 +1,12 @@
 const pool = require('../config/db')
-const { randomInt } = require('crypto')
+const { randomUUID, randomInt } = require('crypto')
 
 afterAll(async () => {
     await pool.end()
 })
 
 const generateTestingEmail = () => {
-    return `testuser_${Date.now()}_${randomInt(1000)}@example.com`
+    return `testuser_${Date.now()}_${randomInt(1000)}_${randomUUID()}@example.com`
 }
 
 module.exports = { generateTestingEmail }
